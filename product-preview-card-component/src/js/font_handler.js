bc020f8469714c5a14707435fca89aba84b35f90
Ayxa Chaverra <achaverrar@gmail.com>
2022-09-25 18:12:35 -0500

    finish setting up webpack config

--- product-preview-card-component/src/js/font_handler.js
+++ product-preview-card-component/src/js/font_handler.js
@@ -3,12 +3,12 @@ if ("fonts" in document) {
     "Monserrat",
     "url(./fonts/montserrat-v25-latin-500.woff2) format('woff2'), url(./fonts/montserrat-v25-latin-500.woff) format('woff')"
   );
-  let M_bold = new FontFace(
+  var M_bold = new FontFace(
     "Monserrat",
     "url(./fonts/montserrat-v25-latin-700.woff2) format('woff2'), url(./fonts/montserrat-v25-latin-700.woff) format('woff')",
     { weight: "700" }
   );
-  let F_bold = new FontFace(
+  var F_bold = new FontFace(
     "Fraunces",
     "url(./fonts/fraunces-v24-latin-700.woff2) format('woff2'), url(./fonts/fraunces-v24-latin-700.woff) format('woff')",
     { weight: "700" }
@@ -25,7 +25,7 @@ if ("fonts" in document) {
 
 if (!("fonts" in document) && "head" in document) {
   // Awkwardly dump the second stage @font-face blocks in the head
-  let style = document.createElement("style");
+  var style = document.createElement("style");
   // Note: Edge supports WOFF2
   style.innerHTML =
     "@font-face { font-family: Monserrat; src: url(./fonts/montserrat-v25-latin-500.woff2) format('woff2'), url(./fonts/montserrat-v25-latin-500.woff) format('woff'); } @font-face { font-family: Monserrat; font-weight: 700; src: url(./fonts/montserrat-v25-latin-500.woff2) format('woff2'), url(./fonts/montserrat-v25-latin-500.woff) format('woff'); } @font-face { font-family: Fraunces; font-weight: 700; src: url(./fonts/fraunces-v24-latin-700.woff2) format('woff2'), url(./fonts/fraunces-v24-latin-700.woff) format('woff'); }";
